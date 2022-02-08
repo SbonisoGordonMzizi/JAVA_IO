ede33e9dd9ab59ae28df088f247b69de5f274841
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2022-02-08 22:03:40 +0200

    write4

---
+++ write4.java
@@ -0,0 +1,13 @@
+package com.tech;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.IOException;
+
+public class write4 {
+    public static void main(String... args){
+        
+    }
+    
+}
